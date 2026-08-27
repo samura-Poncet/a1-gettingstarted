ae3ad922fe8f336d9c65d9031dcc860b56bce307
Samyyyyyyyyyyyyyyyyyyyyyyyyyyy <poncetdja55@gmail.com>
2026-08-27 16:26:22 -0400

    add pages and image

--- server.js
+++ server.js
@@ -10,6 +10,31 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '2page.html':
+      sendFile(response, '2page.html')
+      break
+    case '3page.html':
+      sendFile(response, '3page.html')
+      break
+    case 'cat2.png':
+      sendFile(response, 'cat2.png')
+      break
+    case 'crochet.png':
+      sendFile(response, 'crochet.png')
+      break
+    case 'powerlifting.webp':
+      sendFile(response, 'powerlifting.webp')
+      break
+    case 'realcat.png':
+      sendFile(response, 'realcat.png')
+      break
+    case 'swimming.png':
+      sendFile(response, 'swimming.png')
+      break
+    case 'walking.png':
+      sendFile(response, 'walking.png')
+      break
+
     default:
       response.end( '404 Error: File Not Found' )
   }
